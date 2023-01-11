27a5dfb8983fcf81389e61fb92373efd7242ea9c
Jordan Layne <jordanmlayne2001@gmail.com>
2023-01-11 03:44:54 -0500

    Project 4

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,15 @@ const initialCards = [
 
 const btnEdit = document.querySelector("#btn-edit");
 const profileModal = document.querySelector(".modal");
-
+const profileForm = document.querySelector('.form')
+const btnSubmit = document.querySelector("#btn-submit");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+const inputName = document.querySelector(".form__input-title");
+const inputJob = document.querySelector(".form__input-description");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 function openModal() {
   profileModal.classList.remove("modal_closed");
 }
@@ -39,3 +47,31 @@ function closeModal() {
   profileModal.classList.add("modal_closed");
 }
 profileModalCloseButton.addEventListener("click", closeModal);
+
+function formSubmit(evt) {
+  evt.preventDefault();
+
+ const profileEntry = inputName.value;
+  const JobEntry = inputJob.value;
+  profileName.textContent = profileEntry;
+  profileJob.textContent = JobEntry;
+
+  closeModal()
+}
+
+btnSubmit.addEventListener('click', formSubmit)
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardHeading = cardElement.querySelector(".card__heading");
+  cardImage.alt = cardData.name;
+  cardImage.src = cardData.link;
+  cardHeading.textContent = cardData.name;
+  return cardElement;}
+
+  initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardList.prepend(cardElement);
+  });
+ 
